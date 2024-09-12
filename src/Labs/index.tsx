e7e988700a5a1fb1a98b19a9e7669513d351dc09
chenchen218 <chenchen218@utexas.edu>
2024-09-11 22:56:06 -0700

    a1 lab

---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+import Lab1 from "./Lab1";
+import Lab2 from "./lab2";
+export default function Labs(){
+    return (
+    <div>
+        <h1>labs</h1>
+        <Lab1 />
+        <Lab2 />
+        <h2> lab 3</h2>
+    </div>
+    );
+}
