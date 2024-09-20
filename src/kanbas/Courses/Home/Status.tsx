89950fa69c057b876fe40f3146702ba3d58174d2
chenchen218 <chenchen218@utexas.edu>
2024-09-20 11:33:23 -0700

    Arranged the buttons

--- src/kanbas/Courses/Home/Status.tsx
+++ src/kanbas/Courses/Home/Status.tsx
@@ -2,22 +2,42 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <div>
+      {/* <div>
+        <br />
         <button>Collapse All</button>
         <button>View Progress</button>
-        <button>Publish ALl</button>
+        <select id="wd-select-one-option">
+          <option value="PUBLISH_ALL">Publish All</option>
+          <option value="ASSIGNMENTS">Assignments</option>
+          <option selected value="PROJECTS">
+            Projects
+          </option>
+          <option value="QUIZZES">Quizzes</option>
+          <option value="EXAMS">Exams</option>
+        </select>
         <button>+ Module</button>
+        <br />
+      </div> */}
+      <div className="Status buttons">
+        <button>Unpublish</button>
+        <button>Publish</button>
+        <br />
+        <br />
+        <button>Import Exsiting Content</button>
+        <br />
+        <button>Import from Commons</button>
+        <br />
+        <button>Choose home page</button>
+        <br />
+        <button>View course stream</button>
+        <br />
+        <button>New Announcement</button>
+        <br />
+        <button>New Analytics</button>
+        <br />
+        <button>View Course Notifications</button>
+        <br />
       </div>
-
-      <button>Unpublish</button>
-      <button>Publish</button>
-      <button>Import Exsiting Content</button>
-      <button>Import from Commons</button>
-      <button>Choose home page</button>
-      <button>View course stream</button>
-      <button>New Announcement</button>
-      <button>New Analytics</button>
-      <button>View Course Notifications</button>
     </div>
   );
 }
--- src/kanbas/Courses/Modules/index.tsx
+++ src/kanbas/Courses/Modules/index.tsx
@@ -1,6 +1,22 @@
 export default function Modules() {
   return (
     <div>
+      <div>
+        <br />
+        <button>Collapse All</button>
+        <button>View Progress</button>
+        <select id="wd-select-one-option">
+          <option selected value="PUBLISH_ALL">
+            Publish All
+          </option>
+          <option value="ASSIGNMENTS">Assignments</option>
+          <option value="PROJECTS">Projects</option>
+          <option value="QUIZZES">Quizzes</option>
+          <option value="EXAMS">Exams</option>
+        </select>
+        <button>+ Module</button>
+        <br />
+      </div>
       {/* Implement Collapse All button, View Progress button, etc. */}
       <ul id="wd-modules">
         <li className="wd-module">
@@ -19,7 +35,7 @@ export default function Modules() {
             </li>
 
             <li className="wd-lesson">
-              <span className="wd-title"> Reading</span>
+              <span className="wd-title"> READINGS</span>
               <ul className="wd-content">
                 <li className="wd-content-item">
                   Full Stack Developer- Chapter 1 - Introduction
@@ -31,7 +47,7 @@ export default function Modules() {
             </li>
 
             <li className="wd-lesson">
-              <span className="wd-title"> Slides</span>
+              <span className="wd-title"> SLIDES</span>
               <ul className="wd-content">
                 <li className="wd-content-item">
                   Introduction to Web Development
