cb1e2cb74c501885faff60720035cf39fe338f66
chenchen218 <chenchen218@utexas.edu>
2024-09-28 19:04:33 -0700

    a2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import "./corners";
+import "./margins";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -42,6 +44,8 @@ export default function Lab2() {
         style={{
           backgroundColor: "blue",
           color: "white",
+          padding: "10px",
+          marginTop: "50px",
         }}
       >
         Headings ... width
@@ -53,6 +57,12 @@ export default function Lab2() {
         style={{
           backgroundColor: "red",
           color: "white",
+          borderStyle: "solid",
+          borderBottomWidth: "10px",
+          borderTopWidth: "20px",
+          borderLeftWidth: "30px",
+          borderRightWidth: "40px",
+          borderColor: "green",
         }}
       >
         Span elements
@@ -66,6 +76,48 @@ export default function Lab2() {
         render inline
       </span>
       with the rest of the content
+      <p
+        className="wd-border-fat wd-border-red
+                wd-border-solid"
+      >
+        Solid fat red border
+      </p>
+      <p
+        className="wd-border-thin wd-border-blue
+                wd-border-dashed"
+      >
+        Dashed thin blue border
+      </p>
+      {/* #slides 15 */}
+      <div id="wd-css-margins">
+        <h2>Margins</h2>
+        <div
+          className="wd-margin-bottom wd-padded-top-left 
+      wd-border-fat wd-border-red wd-border-solid 
+      wd-bg-color-yellow"
+        >
+          Margin bottom{" "}
+        </div>
+        <div
+          className="wd-margin-right-left wd-padded-bottom-right 
+      wd-border-fat wd-border-blue  wd-border-solid 
+      wd-bg-color-yellow"
+        >
+          Margin left right{" "}
+        </div>
+        <div
+          className="wd-margin-all-around 
+      wd-padding-fat wd-border-fat 
+      wd-border-yellow wd-border-solid 
+      wd-bg-color-blue wd-fg-color-white"
+        >
+          Margin all around{" "}
+        </div>
+      </div>
+      <h3>Rounded Corners</h3>
+      <p className="rounded-corners-top botder-thin border-blue border-solid padding-fat">
+        Rounded corners on the top
+      </p>
     </div>
   );
 }
