640c6a255af874ed7781ce21ca4048ba94f3abe5
chenchen218 <chenchen218@utexas.edu>
2024-10-02 00:14:35 -0700

    lab1 updated

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -29,6 +29,23 @@ export default function Lab1() {
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book
         </p>
+        <p id="wd-p-1"> ... </p>
+        <p id="wd-p-2">
+          {" "}
+          This is the first paragraph. The paragraph tag is used to format
+          vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+          This is the second paragraph. Even though there is a deliberate white
+          gap between the paragraph above and this paragraph, by default
+          browsers render them as one contiguous piece of text as shown here on
+          the right.
+        </p>
+        <p id="wd-p-4">
+          This is the third paragraph. Wrap each paragraph with the paragraph
+          tag to tell browsers to render the gaps.
+        </p>
+        g
         <div id="wd-lists">
           <h4>List Tags</h4>
           <h5>Ordered List Tag</h5>
