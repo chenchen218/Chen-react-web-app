e5d5ff43823ccc88dc37581c5c5a95ded133d1d0
chenchen218 <chenchen218@utexas.edu>
2024-10-20 16:22:40 -0700

    assignments layout

--- src/kanbas/Courses/Assignments/index.tsx
+++ src/kanbas/Courses/Assignments/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useParams, Link } from "react-router-dom";
 import AssignmentCheck from "./AssignmentCheckButton";
 import AssignmentSearchBar from "./AssignmentSearch";
-import { BsGripVertical } from "react-icons/bs";
-import { FaPlus } from "react-icons/fa";
+import { BsGripVertical } from "react-icons/bs"; // Icon for drag handle
+import { FaPlus, FaBook } from "react-icons/fa"; // Book icon
 import * as db from "../../Database"; // Import the assignments data
 
 export default function Assignments() {
@@ -13,8 +13,10 @@ export default function Assignments() {
   ); // Filter assignments for the current course
 
   return (
-    <div id="wd-assignments">
-      <AssignmentSearchBar />f
+    <div id="wd-assignments" className="px-4">
+      {" "}
+      {/* Added padding to the container */}
+      <AssignmentSearchBar />
       <button className="btn btn-lg btn-danger float-end">
         <FaPlus className="me-2" />
         Add Assignment
@@ -23,14 +25,16 @@ export default function Assignments() {
       <br />
       <br />
       <ul id="wd-assignments-titles" className="list-group rounded-0">
-        <li className="wd-assignments-title list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" />
-            ASSIGNMENTS
+        <li className="wd-assignments-title list-group-item p-0 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <BsGripVertical className="me-3 fs-3" />{" "}
+              {/* Increased margin-right for more space */}
+              ASSIGNMENTS
+            </div>
             <span className="float-end badge rounded-pill text-bg-primary">
               40% of Total
             </span>
-            <BsGripVertical className="me-2 fs-3 float-end" />
           </div>
 
           <ul className="wd-assignment list-group rounded-0">
@@ -39,22 +43,38 @@ export default function Assignments() {
               assignments.map((assignment) => (
                 <li
                   key={assignment._id}
-                  className="wd-assignment list-group-item p-3 ps-1"
+                  className="wd-assignment list-group-item p-4"
+                  style={{
+                    paddingLeft: "20px", // Ensure padding does not interfere with border
+                    border: "1px solid #ddd", // Main border around the item
+                    borderLeft: "4px solid green", // Green left border
+                    marginBottom: "0", // Remove margin between assignments
+                    borderTop: "none", // Remove top border to eliminate double lines
+                  }}
                 >
-                  <BsGripVertical className="me-2 fs-3" />
-                  <Link
-                    className="wd-assignment-link"
-                    to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
-                  >
-                    {assignment.title} <AssignmentCheck />
-                  </Link>
-                  <span className="float-end">
-                    <br />
-                    {assignment.description || "No description available"}{" "}
-                    <br />
-                    <strong>Due:</strong> {assignment.dueDate || "TBD"} <br />
-                    <strong>Points:</strong> {assignment.points || "N/A"} pts
-                  </span>
+                  <div className="d-flex justify-content-between align-items-center mb-3">
+                    {" "}
+                    {/* Adjusted for better layout */}
+                    <div className="d-flex align-items-center">
+                      <BsGripVertical className="me-3 fs-3" /> {/* Drag icon */}
+                      <FaBook className="me-3 text-success" />{" "}
+                      {/* Green Book icon */}
+                      <Link
+                        className="wd-assignment-link fs-5"
+                        to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
+                      >
+                        {assignment.title}
+                      </Link>
+                    </div>
+                    <AssignmentCheck />
+                  </div>
+                  <div className="text-muted ms-5">
+                    <span className="text-danger">Multiple Modules</span> |{" "}
+                    <strong>Not available until</strong>{" "}
+                    {assignment.availableFrom} |<br />
+                    <strong>Due:</strong> {assignment.dueDate} at 11:59pm |{" "}
+                    <strong>Points:</strong> {assignment.points} pts
+                  </div>
                 </li>
               ))
             ) : (
