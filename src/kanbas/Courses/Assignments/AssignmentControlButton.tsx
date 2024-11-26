84867ab70223de7ced35ea14160b04e3443c46f1
chenchen218 <chenchen218@utexas.edu>
2024-11-25 17:53:37 -0800

    branch 4.1 as a nupdate version of a4

--- src/kanbas/Courses/Assignments/AssignmentControlButton.tsx
+++ src/kanbas/Courses/Assignments/AssignmentControlButton.tsx
@@ -1,10 +1,16 @@
 import { FaPlus } from "react-icons/fa6";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function AssignmentsControlButton() {
+  const navigate = useNavigate();
+  const { cid } = useParams();
+
   return (
     <div id="wd-assignments-controls" className="text-nowrap">
       <button
         id="wd-add-assignments-btn"
         className="btn btn-lg btn-danger me-1 float-end"
+        onClick={() => navigate(`/Kanbas/Courses/${cid}/Assignments/new`)}
       >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Assignment
---
+++ src/kanbas/Courses/Assignments/client.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
+const COURSES_URL = `${API_BASE}/courses`;
+const ASSIGNMENTS_URL = `${API_BASE}/assignments`;
+
+export const findAssignmentsForCourse = async (courseId: string) => {
+  const response = await axios.get(`${COURSES_URL}/${courseId}/assignments`);
+  return response.data;
+};
+
+export const createAssignment = async (courseId: string, assignment: any) => {
+  const response = await axios.post(
+    `${COURSES_URL}/${courseId}/assignments`,
+    assignment
+  );
+  return response.data;
+};
+
+export const deleteAssignment = async (assignmentId: string) => {
+  const response = await axios.delete(`${ASSIGNMENTS_URL}/${assignmentId}`);
+  return response.status;
+};
+
+export const updateAssignment = async (assignment: any) => {
+  const response = await axios.put(
+    `${ASSIGNMENTS_URL}/${assignment._id}`,
+    assignment
+  );
+  return response.data;
+};
